36f0675d1c19cae895b4fba4a76f45c22053ea3f
Senior Coder <agilecoder2040@gmail.com>
2024-07-23 05:00:28 +0900

    d

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -2,8 +2,8 @@ import "./Loading.css";
 import "../css/font.css";
 const Loading = () => {
   return (
-    <div className="w-screen h-screen" style={{backgroundImage: "url(/image/loading/cashtree_loading.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-
+    <div className="w-screen h-screen flex justify-center items-center" style={{ backgroundImage: "url(/image/loading/cashtree_loading.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+      <div className="loader"></div>
     </div>
   );
 };
