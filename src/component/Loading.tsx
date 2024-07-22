3d311d3a8110674d97fea689368e2bbd15b903e7
Senior Coder <agilecoder2040@gmail.com>
2024-07-23 04:41:02 +0900

    d

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -2,7 +2,7 @@ import "./Loading.css";
 import "../css/font.css";
 const Loading = () => {
   return (
-    <div className="w-screen h-screen bg-[#4F2596]">
+    <div className="w-screen h-screen" style={{background: "linear-gradient(to top, #49208D, #7845DB);"}}>
       <img src="image/loading/cashtree-loading.png" alt="" className="w-full h-auto"/>
 
     </div>
