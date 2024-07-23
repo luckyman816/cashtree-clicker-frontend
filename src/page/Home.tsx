f88ab90b7dab7dca1b0e0fdc1aa6115e70d12016
Senior Coder <agilecoder2040@gmail.com>
2024-07-23 13:13:23 +0900

    d

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -183,7 +183,7 @@ function Home() {
         <div className="flex justify-center items-center absolute w-[356px] h-[347px] z-10 bg-cover bg-no-repeat translate-y-[-50px]" style={{backgroundImage: "url('image/tap-image/cashtree_bg.png')"}}>
         </div>
         <img
-          className={`rounded-full bg-cover z-50 w-[85%] h-[95%] ${remainedEnergy > 0
+          className={`rounded-full bg-cover z-50 w-[80%] h-[100%] ${remainedEnergy > 0
             ? "cursor-pointer"
             : "cursor-not-allowed opacity-50 top-[80px]"
             } ${imgStatus ? " border-[5px]" : "border-0"}
