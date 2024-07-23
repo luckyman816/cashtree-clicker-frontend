e24b6e567567b9beab5c4b87f0b7b0e7757dde94
Senior Coder <agilecoder2040@gmail.com>
2024-07-23 13:45:00 +0900

    d\

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -180,10 +180,10 @@ function Home() {
       </div>
 
       <div className="flex justify-center items-center relative h-[40vh] w-full">
-        <div className="flex justify-center items-center absolute w-[356px] h-[347px] z-10 bg-cover bg-no-repeat translate-y-[-50px]" style={{backgroundImage: "url('image/tap-image/cashtree_bg.png')"}}>
-        </div>
+        {/* <div className="flex justify-center items-center absolute w-[356px] h-[347px] z-10 bg-cover bg-no-repeat translate-y-[-50px]" style={{backgroundImage: "url('image/tap-image/cashtree_bg.png')"}}>
+        </div> */}
         <img
-          className={`rounded-full bg-cover z-50 w-[80%] h-[100%] ${remainedEnergy > 0
+          className={`rounded-full bg-cover z-50 w-[70%] h-[100%] ${remainedEnergy > 0
             ? "cursor-pointer"
             : "cursor-not-allowed opacity-50 top-[80px]"
             } ${imgStatus ? " border-[5px]" : "border-0"}
