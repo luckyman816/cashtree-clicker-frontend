9270a0338e7d9dccb244d252dfc387d452f8ad48
Senior Coder <agilecoder2040@gmail.com>
2024-07-23 12:29:01 +0900

    update the tap image

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -180,8 +180,8 @@ function Home() {
       </div>
 
       <div className="flex justify-center items-center relative h-[40vh]">
-        {/* <div className="bg-[url('image/tap-image/cashtree_bg.png')] flex justify-center items-center absolute w-[365px] h-[347px]">
-        </div> */}
+        <div className="flex justify-center items-center absolute w-[365px] h-[347px] z-10" style={{backgroundImage: "url('image/tap-image/cashtree_bg.png')"}}>
+        </div>
         <div
           className={`rounded-full bg-cover z-50 w-[360px] h-[365px] ${remainedEnergy > 0
             ? "cursor-pointer"
