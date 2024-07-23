5af1992b889583e9f884a88f5dbdd7ef11764fd7
Senior Coder <agilecoder2040@gmail.com>
2024-07-23 14:02:14 +0900

    d

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -180,7 +180,7 @@ function Home() {
       </div>
 
       <div className="flex justify-center items-center relative h-[45vh] w-full">
-        <img className="flex justify-center items-center absolute w-auto h-[100%] z-10 bg-cover bg-no-repeat translate-y-[-50px]" src="/image/tap-image/cashtree_bg.png">
+        <img className="flex justify-center items-center absolute w-auto h-[80%] z-10 bg-cover bg-no-repeat translate-y-[-50px]" src="/image/tap-image/cashtree_bg.png">
         </img>
         <img
           className={`rounded-full bg-cover z-50 w-auto h-[100%] ${remainedEnergy > 0
