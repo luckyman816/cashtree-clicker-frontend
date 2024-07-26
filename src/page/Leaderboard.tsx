0c6ff44b72f9422aed30853693f85ea7eb1902c9
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 11:58:01 +0900

    d

--- src/page/Leaderboard.tsx
+++ src/page/Leaderboard.tsx
@@ -3,20 +3,8 @@ import Leaderboarder from "../component/Leaderboarder";
 export default function Leaderboard() {
     return (
         <div className="flex flex-col justify-between items-center h-full w-full ">
-            <div className="w-[90%] flex flex-col justify-center items-center mt-11 gap-4">
-                <div className="flex justify-between items-center px-3 w-full">
-                    <img src="image/icon/back.png" alt="" className=" w-4 h-4" />
-                    <h3
-                        className="text-sm text-[white]"
-                        style={{ fontFamily: "archivo" }}
-                    >
-                        Cashtree Tap to Win
-                    </h3>
-                    <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
-                </div>
-                <Leaderboarder/>
-                <Footer />
-            </div>
+            <Leaderboarder />
+            <Footer />
         </div>
     )
 }
