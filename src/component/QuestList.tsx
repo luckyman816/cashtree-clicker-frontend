4c792644c920ac93dc43a9e5b3f9424db519c7a4
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 14:41:46 +0900

    d

--- src/component/QuestList.tsx
+++ src/component/QuestList.tsx
@@ -74,7 +74,7 @@ export default function QuestList() {
       </div>
       {
         friends.length > 0 ? (
-          <div className="min-h-[45vh] overflow-y-auto w-full">
+          <div className="min-h-[40vh] overflow-y-auto w-full">
             {
               friends.map((friend) => (
                 <div className="flex w-[95%] justify-between px-3 py-3 items-center rounded-[20px] bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#2A0E57] to-[#7B34EE]">
@@ -96,7 +96,7 @@ export default function QuestList() {
 
           </div>
         ) : (
-          <div className="flex w-full justify-center items-start min-h-[45vh]">
+          <div className="flex w-full justify-center items-start min-h-[40vh]">
             <div className="flex w-[95%] justify-center items-center py-4 bg-[#120F29] rounded-[20px]">
               <p className="text-[#ABA7BA] text-sm">You haven't invited anyone yet</p>
             </div>
