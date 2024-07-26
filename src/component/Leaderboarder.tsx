39ee288418045fc7f5fd3832a7a0da927c342f25
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 12:08:28 +0900

    d

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -13,7 +13,10 @@ export default function Leaderboarder() {
           </h3>
           <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
         </div>
-        <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center gap-5"></div>
+        <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center gap-5">
+            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF]">Leaderboard</div>
+            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF]">Prize Pool</div>
+        </div>
         <div className="flex w-full justify-center items-center">
           <p className="text-sm text-white">You and your friend will receive bonuses</p>
         </div>
