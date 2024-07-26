6b337f1f0cf41e31500915f4fa9e1ce528208922
Senior Coder <agilecoder2040@gmail.com>
2024-07-27 04:13:10 +0900

    d

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -117,22 +117,23 @@ export default function Leaderboarder() {
                             </div>
                         </div>
                     </div>
-                    <div className="min-h-[40vh] flex overflow-auto w-full gap-2 flex-wrap">
-                        {
-                            milestones.map((milestone, index) => (
-
-                                <div key={index} className="flex flex-col justify-center items-center py-2 px-2 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#2A0E58] to-[#7F36F7]">
-                                    <img src={`image/leaderboard/${milestone.imgpath}.png`} alt="" className="w-20 h-20" />
-                                    <h1 className="text-white text-lg font-bold">{milestone.title}</h1>
-                                    <h1 className="text-white text-lg font-bold">{milestone.amount}<span className="text-sm">$CTT</span></h1>
-                                    <ul>
-                                        <li className="text-[#C8A2FB] text-[12px]">{milestone.players}</li>
-                                        <li className="text-[#C8A2FB] text-[12px]">{milestone.status}</li>
-                                    </ul>
-                                    <div className="flex justify-center items-center px-2 py-2 text-white text-sm font-bold rounded-[20px]">More Info</div>
-                                </div>
-                            ))
-                        }
+                    <div className="min-h-[40vh] flex overflow-auto w-full justify-center items-center">
+                        <div className="flex justify-between w-[90%] items-center">
+                            {
+                                milestones.map((milestone, index) => (
+                                    <div key={index} className="flex flex-col justify-center items-center py-2 px-2 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#2A0E58] to-[#7F36F7]">
+                                        <img src={`image/leaderboard/${milestone.imgpath}.png`} alt="" className="w-20 h-20" />
+                                        <h1 className="text-white text-lg font-bold">{milestone.title}</h1>
+                                        <h1 className="text-white text-lg font-bold">{milestone.amount}<span className="text-sm">$CTT</span></h1>
+                                        <ul>
+                                            <li className="text-[#C8A2FB] text-[12px]">{milestone.players}</li>
+                                            <li className="text-[#C8A2FB] text-[12px]">{milestone.status}</li>
+                                        </ul>
+                                        <div className="flex justify-center items-center px-2 py-2 text-white text-sm font-bold rounded-[20px]">More Info</div>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             )
