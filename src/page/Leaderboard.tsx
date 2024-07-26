8973b5ccb3463a0265c59b09048214519beab36d
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 11:47:29 +0900

    d

--- src/page/Leaderboard.tsx
+++ src/page/Leaderboard.tsx
@@ -2,7 +2,7 @@ import Footer from "../component/Footer";
 
 export default function Leaderboard() {
     return (
-        <div className="flex flex-col justify-between items-center h-full w-full">
+        <div className="flex flex-col justify-between items-center h-full w-full mt-11">
             <div className="w-[90%] flex flex-col justify-center items-center gap-4">
                 <div className="flex justify-between items-center px-3 w-full">
                     <img src="image/icon/back.png" alt="" className=" w-4 h-4" />
