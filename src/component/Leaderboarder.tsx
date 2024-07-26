d71e68d49bd27270b0c0075dfc640f05b24acae8
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 12:18:05 +0900

    d

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -13,25 +13,11 @@ export default function Leaderboarder() {
           </h3>
           <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
         </div>
-        <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center gap-5">
-            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF]">Leaderboard</div>
-            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF]">Prize Pool</div>
+        <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center px-5 gap-5">
+            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF] py-3 rounded-[10px]">Leaderboard</div>
+            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF] py-3 rounded-[10px]">Prize Pool</div>
         </div>
-        <div className="flex w-full justify-center items-center">
-          <p className="text-sm text-white">You and your friend will receive bonuses</p>
-        </div>
-        <div className="flex w-[95%] justify-between px-3 py-3 items-center rounded-[20px] bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#AA45FE]">
-          <div className="flex justify-center items-center gap-3">
-            <img src="image/friends/friends.png" alt="" className="w-10 h-10" />
-            <div className="flex flex-col justify-start items-start">
-              <h1 className="text-white text-xl font-bold">Invite Friends</h1>
-              <h1 className="text-white text-sm ">For you and your friend</h1>
-            </div>
-          </div>
-          <div className="flex justify-center items-center gap-2">
-            <img src="image/assets/coin.png" alt="coin" className="w-5 h-5" />
-            <h1 className="text-[#FFC107] text-sm">+25.000</h1>
-          </div>
-        </div>
+        
+       
       </div>)
 }
