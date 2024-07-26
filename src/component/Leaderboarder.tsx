06c6d3f9cbf57a398d8004c42be7dad3a735f763
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 13:00:01 +0900

    d

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -1,5 +1,24 @@
 import { /*toast,*/ ToastContainer } from "react-toastify";
+import { useState } from "react";
 export default function Leaderboarder() {
+    const Players=[
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+        {name:"User1",coin:"25.000"},
+    ]
+    const [isLeaderboardOpen, setIsLeaderboardOpen] = useState(true);
+    const handleLeaderboardClick = () => {
+        setIsLeaderboardOpen(true);
+    }
+    const handlePrizePoolClick = () => {
+        setIsLeaderboardOpen(false);
+    }
     return (
         <div className="p-5 flex flex-col justify-center items-center gap-5 w-full">
         <ToastContainer />
@@ -14,10 +33,33 @@ export default function Leaderboarder() {
           <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
         </div>
         <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center px-5 gap-5">
-            <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-3 rounded-[10px]">Leaderboard</div>
-            <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-3 rounded-[10px]">Prize Pool</div>
+            <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-3 rounded-[10px]" onClick={()=>handleLeaderboardClick()}>Leaderboard</div>
+            <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-3 rounded-[10px]" onClick={()=>handlePrizePoolClick()}>Prize Pool</div>
         </div>
         <div className="text-white text-[32px] justify-center font-[Archivo] items-center">Leaderboard</div>
         <div className="text-white text-[12px] justify-center items-center">Leaderboard & rank of worldwide players</div>
+        { isLeaderboardOpen?(
+        <div className="min-h-[45vh] flex overflow-auto w-full">
+            {Players.map((player, index) => (
+                <div key={index} className="flex justify-between items-center w-full bg-gradient-to-br from-[#8137F9] to-[#240C4D]">
+                    <div className="flex-[1]">
+                        <img src="image/leaderboard/playerIcon.png" alt="" className="w-[42px] h-[42px]"/>
+                    </div>
+                    <div className="flex-[5] flex-col justify-start items-center">
+                        <div className="text-white text-[12px]">{player.name}</div>
+                        <div className="flex justify-start items-center">
+                            <img src="image/leaderboard/coin.png" alt="" />
+                            <div>{player.coin}</div>
+                        </div>
+                    </div>
+                    <div className="flex-[2] text-2xl text-white">{index}</div>
+                </div>
+            ))}
+        </div>
+        ):
+
+        (<div className="min-h-[45vh] flex overflow-auto w-full">
+        </div>)
+        }
       </div>)
 }
