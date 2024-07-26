ab6a0e0850277e946a8a3b161b35984ecee3526f
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 12:29:55 +0900

    d

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -1,7 +1,7 @@
 import { /*toast,*/ ToastContainer } from "react-toastify";
 export default function Leaderboarder() {
     return (
-        <div className="p-5 flex flex-col justify-center items-center gap-4 w-full">
+        <div className="p-5 flex flex-col justify-center items-center gap-5 w-full">
         <ToastContainer />
         <div className="flex justify-between items-center px-3 w-full">
           <img src="image/icon/back.png" alt="" className=" w-4 h-4" />
@@ -13,13 +13,11 @@ export default function Leaderboarder() {
           </h3>
           <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
         </div>
-        <div className="flex gap-3">
         <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center px-5 gap-5">
-            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF] py-3 rounded-[10px]">Leaderboard</div>
-            <div className="w-2/3 bg-[#120F29] font-bold text-sm text-white active:bg-[#7520FF] py-3 rounded-[10px]">Prize Pool</div>
+            <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-3 rounded-[10px]">Leaderboard</div>
+            <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-3 rounded-[10px]">Prize Pool</div>
         </div>
         <div className="text-white text-[32px] justify-center items-center">Leaderboard</div>
-        </div>
         
       </div>)
 }
