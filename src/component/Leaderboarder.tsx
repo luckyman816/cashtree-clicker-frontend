7afe9625dd0adc5385aa00a700b5b27de7bb9353
Senior Coder <agilecoder2040@gmail.com>
2024-07-26 16:41:30 +0900

    update the change

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -33,8 +33,8 @@ export default function Leaderboarder() {
                 <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
             </div>
             <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center px-5 gap-5">
-                <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-2 rounded-[10px]" onClick={() => handleLeaderboardClick()}>Leaderboard</div>
-                <div className="w-2/3 bg-[#120F29]  text-lg text-white active:bg-[#7520FF] py-2 rounded-[10px]" onClick={() => handlePrizePoolClick()}>Prize Pool</div>
+                <div className={`w-2/3  text-lg text-white ${isLeaderboardOpen ? "bg-[#7520FF]" : "bg-[#120F29]"} py-2 rounded-[10px]`} onClick={() => handleLeaderboardClick()}>Leaderboard</div>
+                <div className={`w-2/3  text-lg text-white ${!isLeaderboardOpen ? "bg-[#7520FF]" : "bg-[#120F29]"} py-2 rounded-[10px]`} onClick={() => handlePrizePoolClick()}>Prize Pool</div>
             </div>
             <div className="text-white text-[32px] justify-center font-[Archivo] items-center">Leaderboard</div>
             <div className="text-white text-[12px] justify-center items-center">Leaderboard & rank of worldwide players</div>
