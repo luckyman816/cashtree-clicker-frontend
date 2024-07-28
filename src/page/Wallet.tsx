b569b7a0acd619a9d3b98c187ae60b30f3b7bae7
Senior Coder <agilecoder2040@gmail.com>
2024-07-29 02:32:06 +0900

    d

--- src/page/Wallet.tsx
+++ src/page/Wallet.tsx
@@ -1,12 +1,10 @@
-import { ToastContainer } from "react-toastify";
+
 import Footer from "../component/Footer";
 export default function Wallet() {
 
   return (
-    <div className="flex flex-col justify-between items-center h-full w-full">
-      <ToastContainer />
-
+    <>
       <Footer />
-    </div>
+    </>
   );
 }
