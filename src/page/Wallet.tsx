f2c83e4e4004d2bdeb81bf456da9618c97b63ed9
Senior Coder <agilecoder2040@gmail.com>
2024-07-29 02:47:39 +0900

    d

--- src/page/Wallet.tsx
+++ src/page/Wallet.tsx
@@ -1,9 +1,10 @@
-
+import Wallet from "../component/Wallet";
 import Footer from "../component/Footer";
 export default function Wallet() {
 
   return (
     <>
+      <Wallet />
       <Footer />
     </>
   );
