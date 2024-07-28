3cfe4c00459f5b38e3a0194ab479f033492d434d
Senior Coder <agilecoder2040@gmail.com>
2024-07-29 03:23:56 +0900

    d

--- src/component/Wallet.tsx
+++ src/component/Wallet.tsx
@@ -21,14 +21,14 @@ export default function Wallet() {
             <div className="flex flex-col w-full justify-start items-center gap-4">
                 <div className="flex justify-start text-white text-[17px] font-[Archivo]">Task List</div>
                 <div className="flex flex-row justify-between items-center bg-gradient-to-br from-[#AE47FF] to-[#6929F1] w-full h-16 rounded-2xl px-1">
-                    <div className="flex-1 flex">
+                    <div className="flex-1 flex justify-center items-center">
                         <img src="image/wallet/listIcon.png" alt="" className=" w-11 h-11" />
                     </div>
-                    <div className="flex-5 flex flex-col gap-1 justify-start items-center">
+                    <div className="flex-7 flex flex-col gap-1 justify-start items-center">
                         <div className="text-white font-[Archivo] text-sm">Connect your CTT wallet address</div>
                         <div className="text-white font-[Archivo] text-xs">Integrate now for Secure Transactions</div>
                     </div>
-                    <div className="flex-1 flex">
+                    <div className="flex-1 flex justify-end">
                         <img src="image/wallet/Vector.png" alt="" className="" />
                     </div>
                 </div>
