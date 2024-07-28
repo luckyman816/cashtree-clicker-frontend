e9976b3f3ea9433555c4368faa75fd372a58281e
Senior Coder <agilecoder2040@gmail.com>
2024-07-29 03:20:23 +0900

    d

--- src/component/Wallet.tsx
+++ src/component/Wallet.tsx
@@ -13,22 +13,26 @@ export default function Wallet() {
                 </h3>
                 <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
             </div>
-        <div className="flex flex-col w-full justify-center items-center gap-2">
-            <img src="image/wallet/cointap.png" alt="" className="w-60 h-60"/>
-            <div className="text-3xl text-white font-[Archivo] font-bold">Wallet</div>
-            <div className="text-sm text-white font-[Archivo]">Your wallet connect address</div>
-        </div>
-        <div className="flex flex-col w-full justify-start items-center gap-4">
-            <div className="flex justify-start text-white text-[17px] font-[Archivo]">Task List</div>
-            <div className="flex flex-row justify-between items-center bg-gradient-to-br from-[#AE47FF] to-[#6929F1] w-full h-16 rounded-2xl ">
-                <img src="image/wallet/listIcon.png" alt="" className="flex-1 w-11 h-11"/>
-                <div className="flex-5 flex flex-col gap-1 justify-start items-center">
-                    <div className="text-white font-[Archivo] text-sm">Connect your CTT wallet address</div>
-                    <div className="text-white font-[Archivo] text-xs">Integrate now for Secure Transactions</div>
-                </div>
-                <img src="image/wallet/Vector.png" alt="" className="flex-1 flex"/>
+            <div className="flex flex-col w-full justify-center items-center gap-2">
+                <img src="image/wallet/cointap.png" alt="" className="w-48 h-48" />
+                <div className="text-3xl text-white font-[Archivo] font-bold">Wallet</div>
+                <div className="text-sm text-white font-[Archivo]">Your wallet connect address</div>
             </div>
-        </div>
+            <div className="flex flex-col w-full justify-start items-center gap-4">
+                <div className="flex justify-start text-white text-[17px] font-[Archivo]">Task List</div>
+                <div className="flex flex-row justify-between items-center bg-gradient-to-br from-[#AE47FF] to-[#6929F1] w-full h-16 rounded-2xl ">
+                    <div className="flex-1 flex">
+                        <img src="image/wallet/listIcon.png" alt="" className=" w-11 h-11" />
+                    </div>
+                    <div className="flex-5 flex flex-col gap-1 justify-start items-center">
+                        <div className="text-white font-[Archivo] text-sm">Connect your CTT wallet address</div>
+                        <div className="text-white font-[Archivo] text-xs">Integrate now for Secure Transactions</div>
+                    </div>
+                    <div className="flex-1 flex">
+                        <img src="image/wallet/Vector.png" alt="" className="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
