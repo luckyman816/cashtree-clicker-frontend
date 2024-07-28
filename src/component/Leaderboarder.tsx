da266fb88ff147322825e2b4a1eab27d2b19a6ab
Senior Coder <agilecoder2040@gmail.com>
2024-07-28 10:38:53 +0900

    update the milestone page hegith

--- src/component/Leaderboarder.tsx
+++ src/component/Leaderboarder.tsx
@@ -160,7 +160,7 @@ export default function Leaderboarder() {
                         </div>
                     </div>
                     <div className="flex  w-full justify-center items-center">
-                        <div className="flex justify-between w-[90%] items-center flex-wrap min-h-[40vh] max-h-[40vh] overflow-y-auto">
+                        <div className="flex justify-between w-[90%] items-center flex-wrap min-h-[60vh] max-h-[60vh] overflow-y-auto">
                             {
                                 milestones.map((milestone, index) => (
                                     <div key={index} className="flex flex-col justify-center items-center py-2 px-4 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#2A0E58] to-[#7F36F7] border border-[#885ECE] rounded-[20px]">
