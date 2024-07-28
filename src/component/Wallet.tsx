4a1c7f0385077d3b7d232e261e3da676b237a8bf
Senior Coder <agilecoder2040@gmail.com>
2024-07-29 02:45:56 +0900

    d

--- src/component/Wallet.tsx
+++ src/component/Wallet.tsx
@@ -13,7 +13,11 @@ export default function Wallet() {
                 </h3>
                 <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
             </div>
-
+        <div className="flex flex-col w-full justify-center items-center gap-3">
+            <img src="image/wallet/cointap.png" alt="" className="w-32 h-32"/>
+            <div className="text-3xl text-white font-[Archivo]">Wallet</div>
+            <div className="text-sm text-white font-[Archivo]">Your wallet connect address</div>
+        </div>
         </div>
     )
 }
