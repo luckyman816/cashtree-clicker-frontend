cf7dc787b45e2e00463edbe3990d6718d3a98579
Senior Coder <agilecoder2040@gmail.com>
2024-07-29 10:56:08 +0900

    update the font style

--- src/page/Wallet.tsx
+++ src/page/Wallet.tsx
@@ -2,11 +2,16 @@ import Footer from "../component/Footer";
 import { ToastContainer } from "react-toastify"
 import { useState } from "react"
 import "../css/font.css"
+import Modal from "../component/modal";
 export default function WalletPage() {
-  const [isInviteModal, setIsInviteModal] = useState<boolean>(false)
-  const handleInviteModalClick = () => {
-    return setIsInviteModal(true)
+  const [isWalletModal, setIsWalletModal] = useState<boolean>(false)
+  const handleCloseWalletModal = () => {
+    setIsWalletModal(false)
   }
+  const handleOpenWalletModal = () => {
+    return setIsWalletModal(true)
+  }
+
   return (
     <div className="flex flex-col justify-between items-center h-full w-full">
       <ToastContainer />
@@ -28,8 +33,7 @@ export default function WalletPage() {
         </div>
         <div className="flex flex-col w-full justify-start items-center gap-4">
           <div className="flex justify-start text-white text-[17px] font-[Archivo]">Task List</div>
-          <div className="flex flex-row justify-between items-center bg-gradient-to-br from-[#AE47FF] to-[#6929F1] w-full h-16 rounded-2xl px-1" onClick={() => handleInviteModalClick()}>
-
+          <div className="flex flex-row justify-between items-center bg-gradient-to-br from-[#AE47FF] to-[#6929F1] w-full h-16 rounded-2xl px-1" onClick={() => handleOpenWalletModal()}>
             <div className="flex-7 flex flex-col gap-1 justify-start items-center">
               <div className="text-white font-[Archivo] text-sm">Connect your CTT wallet address</div>
               <div className="text-white font-[Archivo] text-xs">Integrate now for Secure Transactions</div>
@@ -41,6 +45,25 @@ export default function WalletPage() {
         </div>
       </div>
       <Footer />
+      <Modal isOpen={isWalletModal} onClose={handleCloseWalletModal}>
+        <div className="flex flex-col items-center align-middle gap-3 rounded-[20px]">
+          <img src="image/wallet/cointap.png" alt="" className=" w-auto h-[80%]" />
+          <h1 className="text-2xl text-white">Connet your Wallet address</h1>
+          <p className=" text-sm text-white">
+            Connect your crypto wallet. If you don't have one, create it in your Telegram account.
+          </p>
+          <div
+            className="w-[80%] bg-[#7520FF] text-white rounded-[10px] flex justify-center items-center py-3"
+          >
+            <span className="flex justify-center items-center text-white text-xl">Connect Now</span>
+          </div>
+          <div
+            className="w-[80%] text-[#C8A2FB] rounded-[10px] flex justify-center items-center py-3"
+          >
+            <span className="flex justify-center items-center text-[#C8A2FB] text-sm">Cancel</span>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
