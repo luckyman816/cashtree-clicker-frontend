59f5baa495b31c7d83121c297d94481a29ea7778
Senior Coder <agilecoder2040@gmail.com>
2024-07-31 01:07:57 +0900

    d

--- src/page/Leaderboard.tsx
+++ src/page/Leaderboard.tsx
@@ -81,18 +81,18 @@ export default function Leaderboard() {
     }
     return (
         <div className="flex flex-col justify-between items-center h-full w-full ">
+            <div className="flex justify-between items-center px-3 w-full">
+                <img src="image/icon/back.png" alt="" className=" w-4 h-4" />
+                <h3
+                    className="text-sm text-[white]"
+                    style={{ fontFamily: "archivo" }}
+                >
+                    Cashtree Tap to Win
+                </h3>
+                <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
+            </div>
             <div className="p-5 flex flex-col justify-center items-center gap-5 w-full">
                 <ToastContainer />
-                <div className="flex justify-between items-center px-3 w-full">
-                    <img src="image/icon/back.png" alt="" className=" w-4 h-4" />
-                    <h3
-                        className="text-sm text-[white]"
-                        style={{ fontFamily: "archivo" }}
-                    >
-                        Cashtree Tap to Win
-                    </h3>
-                    <img src="image/icon/menu.png" alt="" className=" w-5 h-5" />
-                </div>
                 <div className="flex w-full h-14 rounded-[12px] bg-[#120F29] justify-center items-center px-5 gap-5">
                     <div className={`w-2/3  text-lg text-white ${isLeaderboardOpen ? "bg-[#7520FF]" : "bg-[#120F29]"} py-2 rounded-[10px]`} onClick={() => handleLeaderboardClick()}>Leaderboard</div>
                     <div className={`w-2/3  text-lg text-white ${!isLeaderboardOpen ? "bg-[#7520FF]" : "bg-[#120F29]"} py-2 rounded-[10px]`} onClick={() => handlePrizePoolClick()}>Prize Pool</div>
