4ccc53d5c781993ef8ad21e46f2808184ff80e95
Senior Coder <agilecoder2040@gmail.com>
2024-08-05 10:47:44 +0900

    d

--- src/page/Leaderboard.tsx
+++ src/page/Leaderboard.tsx
@@ -188,72 +188,74 @@ export default function Leaderboard() {
                         </div>
                     </div>
                 ) : (
-                    <div className="flex flex-col justify-center items-center gap-3 w-full min-h-[70vh] max-h-[70vh] overflow-y-auto">
-                        <div className="flex justify-between items-center w-full gap-4">
-                            <h1 className="text-white text-xl font-bold text-left">Welcome to Cashtree Tap-to-Win! <br /><span className="text-[#C8A2FB] font-bold text-xl">Season 1!</span></h1>
-                            <img src="image/leaderboard/season.png" alt="" className="w-16 h-[70px]" />
-                        </div>
-                        <div className="flex justify-start items-center text-white text-sm w-[90%]">8 August - 22 August 2024 <br />Game Status:</div>
-                        <div className="flex w-[95%] px-5 py-2 justify-between items-center rounded-[10px] bg-[#2D2865]">
-                            <div className="flex justify-center items-center gap-2">
-                                <img src="image/leaderboard/seasonUser.png" alt="" className="w-8 h-[26px]" />
-                                <div className="flex flex-col justify-center items-center gap-1">
-                                    <h1 className="text-white text-lg">Total Players</h1>
-                                    <h1 className="text-[#ABA7BA] text-[10px]">Current all players</h1>
-                                </div>
+                    <div className="min-h-[70vh] max-h-[70vh] overflow-y-auto w-full">
+                        <div className="flex flex-col justify-center items-center gap-3 w-full">
+                            <div className="flex justify-between items-center w-full gap-4">
+                                <h1 className="text-white text-xl font-bold text-left">Welcome to Cashtree Tap-to-Win! <br /><span className="text-[#C8A2FB] font-bold text-xl">Season 1!</span></h1>
+                                <img src="image/leaderboard/season.png" alt="" className="w-16 h-[70px]" />
                             </div>
-                            <h1 className="text-[16px] text-white">{formatNumberWithCommas(125350)}</h1>
-                        </div>
-                        <div className="flex flex-col gap-1 w-full justify-start items-start">
-                            <h1 className="text-white text-xl">Milestone 8 With Total Reward</h1>
-                            <h1 className="text-white font-bold text-4xl">{formatNumberWithCommas(75000000)}$CTT</h1>
-                        </div>
-                        <div className="flex gap-3 justify-center items-center">
-                            <div className="flex flex-col gap-2 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#3B1E6A] to-[#7520FF] rounded-[10px] p-5">
-                                <h1 className="text-white text-[12px]">Reward For Leaderboard</h1>
-                                <h1 className="text-white font-bold text-[18px]">{formatNumberWithCommas(25000000)}$CTT</h1>
-                            </div>
-                            <div className="flex flex-col gap-2 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#4517A8] to-[#D940FF] rounded-[10px] p-5">
-                                <h1 className="text-white text-[12px]">Reward For Leaderboard</h1>
-                                <h1 className="text-white font-bold text-[18px]">{formatNumberWithCommas(25000000)}$CTT</h1>
-                            </div>
-                        </div>
-                        <div className="flex flex-col w-[95%] px-5 py-2 justify-between items-center rounded-[10px] bg-[#2D2865]">
-                            <div className="flex justify-between items-center w-full">
-                                <h1 className="text-white text-xl">Current Status</h1>
-                                <img src="image/leaderboard/arrow.png" alt="" className="w-4 h-2" />
-                            </div>
-                            <div className="flex flex-col gap-2 justify-center items-center w-full">
-                                <div className="flex justify-between items-center gap-2 w-full">
-                                    <div className="flex gap-1 justify-center items-center">
-                                        <img src="image/leaderboard/point.png" alt="" className="w-9 h-9" />
-                                        <div className="flex flex-col justify-center items-center gap-1">
-                                            <h1 className="text-white text-lg">Your Points Earned</h1>
-                                            <h1 className="text-[#ABA7BA] text-[10px]">25 August 2024</h1>
-                                        </div>
+                            <div className="flex justify-start items-center text-white text-sm w-[90%]">8 August - 22 August 2024 <br />Game Status:</div>
+                            <div className="flex w-[95%] px-5 py-2 justify-between items-center rounded-[10px] bg-[#2D2865]">
+                                <div className="flex justify-center items-center gap-2">
+                                    <img src="image/leaderboard/seasonUser.png" alt="" className="w-8 h-[26px]" />
+                                    <div className="flex flex-col justify-center items-center gap-1">
+                                        <h1 className="text-white text-lg">Total Players</h1>
+                                        <h1 className="text-[#ABA7BA] text-[10px]">Current all players</h1>
                                     </div>
-                                    <h1 className="text-[16px] text-white">{formatNumberWithCommas(3450899)}</h1>
                                 </div>
-                                <div className="flex justify-between items-center gap-2 w-full">
-                                    <div className="flex justify-center items-center gap-1">
-                                        <img src="image/leaderboard/rank.png" alt="" className="w-9 h-9" />
-                                        <div className="flex flex-col justify-center items-center gap-1">
-                                            <h1 className="text-white text-lg">Your Rank</h1>
-                                            <h1 className="text-[#ABA7BA] text-[10px]">25 August 2024</h1>
-                                        </div>
-                                    </div>
-                                    <h1 className="text-[16px] text-white">{formatNumberWithCommas(1250)}</h1>
+                                <h1 className="text-[16px] text-white">{formatNumberWithCommas(125350)}</h1>
+                            </div>
+                            <div className="flex flex-col gap-1 w-full justify-start items-start">
+                                <h1 className="text-white text-xl">Milestone 8 With Total Reward</h1>
+                                <h1 className="text-white font-bold text-4xl">{formatNumberWithCommas(75000000)}$CTT</h1>
+                            </div>
+                            <div className="flex gap-3 justify-center items-center">
+                                <div className="flex flex-col gap-2 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#3B1E6A] to-[#7520FF] rounded-[10px] p-5">
+                                    <h1 className="text-white text-[12px]">Reward For Leaderboard</h1>
+                                    <h1 className="text-white font-bold text-[18px]">{formatNumberWithCommas(25000000)}$CTT</h1>
+                                </div>
+                                <div className="flex flex-col gap-2 bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#4517A8] to-[#D940FF] rounded-[10px] p-5">
+                                    <h1 className="text-white text-[12px]">Reward For Leaderboard</h1>
+                                    <h1 className="text-white font-bold text-[18px]">{formatNumberWithCommas(25000000)}$CTT</h1>
                                 </div>
                             </div>
-                        </div>
-                        <div className="flex flex-col w-[95%] px-5 py-2 justify-start items-start rounded-[10px] bg-[#2D2865] gap-2">
-                            <h1 className="text-white text-xl">Tips to Boost Your Points</h1>
-                            <ul className="flex flex-col justify-start w-full items-start">
-                                <li className="text-sm text-[#ABA7BA]">Invite Friends:</li>
-                                <li className="text-sm text-[#ABA7BA]">Complete All Missions:</li>
-                                <li className="text-sm text-[#ABA7BA]">Find the Secret Code:</li>
-                                <li className="text-sm text-[#ABA7BA]">Stay Active:</li>
-                            </ul>
+                            <div className="flex flex-col w-[95%] px-5 py-2 justify-between items-center rounded-[10px] bg-[#2D2865]">
+                                <div className="flex justify-between items-center w-full">
+                                    <h1 className="text-white text-xl">Current Status</h1>
+                                    <img src="image/leaderboard/arrow.png" alt="" className="w-4 h-2" />
+                                </div>
+                                <div className="flex flex-col gap-2 justify-center items-center w-full">
+                                    <div className="flex justify-between items-center gap-2 w-full">
+                                        <div className="flex gap-1 justify-center items-center">
+                                            <img src="image/leaderboard/point.png" alt="" className="w-9 h-9" />
+                                            <div className="flex flex-col justify-center items-center gap-1">
+                                                <h1 className="text-white text-lg">Your Points Earned</h1>
+                                                <h1 className="text-[#ABA7BA] text-[10px]">25 August 2024</h1>
+                                            </div>
+                                        </div>
+                                        <h1 className="text-[16px] text-white">{formatNumberWithCommas(3450899)}</h1>
+                                    </div>
+                                    <div className="flex justify-between items-center gap-2 w-full">
+                                        <div className="flex justify-center items-center gap-1">
+                                            <img src="image/leaderboard/rank.png" alt="" className="w-9 h-9" />
+                                            <div className="flex flex-col justify-center items-center gap-1">
+                                                <h1 className="text-white text-lg">Your Rank</h1>
+                                                <h1 className="text-[#ABA7BA] text-[10px]">25 August 2024</h1>
+                                            </div>
+                                        </div>
+                                        <h1 className="text-[16px] text-white">{formatNumberWithCommas(1250)}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col w-[95%] px-5 py-2 justify-start items-start rounded-[10px] bg-[#2D2865] gap-2">
+                                <h1 className="text-white text-xl">Tips to Boost Your Points</h1>
+                                <ul className="flex flex-col justify-start w-full items-start">
+                                    <li className="text-sm text-[#ABA7BA]">Invite Friends:</li>
+                                    <li className="text-sm text-[#ABA7BA]">Complete All Missions:</li>
+                                    <li className="text-sm text-[#ABA7BA]">Find the Secret Code:</li>
+                                    <li className="text-sm text-[#ABA7BA]">Stay Active:</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 )
