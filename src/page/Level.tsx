d3af06cd43259655b632fde2c1198e6d13585ca5
Senior Coder <agilecoder2040@gmail.com>
2024-08-07 04:57:50 +0900

    d

--- src/page/Level.tsx
+++ src/page/Level.tsx
@@ -48,7 +48,7 @@ export default function Level() {
                                 key={index}
                                 className={`flex w-[90%] my-3 px-3 py-3 items-center justify-between bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#A944FD] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)]  rounded-[20px] gap-2 border border-[#B286FA]`}>
                                 <div className="flex justify-center items-center gap-2">
-                                    <img src={`/image/mission/${item.icon}.png`} alt="" className="w-10 h-10" />
+                                    <img src={`/image/level/${item.icon}.png`} alt="" className="w-10 h-10" />
                                     <div className="flex flex-col gap-1 justify-start items-start">
                                         <h1 className="text-sm text-white">{item.name}</h1>
                                         <h1 className="text-[12px] text-white font-bold">
