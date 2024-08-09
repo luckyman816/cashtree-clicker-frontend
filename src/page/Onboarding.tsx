b81ab5465f28534d08e2719d98606446eaf8d8a1
Senior Coder <agilecoder2040@gmail.com>
2024-08-10 05:48:01 +0900

    d

--- src/page/Onboarding.tsx
+++ src/page/Onboarding.tsx
@@ -5,10 +5,10 @@ const Onboarding = () => {
         <div className=" relative w-screen h-screen flex flex-col justify-around items-center bg-[linear-gradient(0deg,_var(--tw-gradient-stops))] from-[#120F29] to-[#7F3AEF]" >
             <div className=" absolute w-full h-full right-0 top-0 z-10" style={{ backgroundImage: "url(/image/onboarding/gradient.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
             </div>
-            <div className="w-full justify-center items-center flex">
+            <div className="w-full justify-center items-center flex z-20">
                 <img src="/image/onboarding/onboarding_1.png" alt="" className="w-[80%] h-auto" />
             </div>
-            <div className="w-full justify-center items-center flex flex-col gap-2">
+            <div className="w-full justify-center items-center flex flex-col gap-2 z-20">
                 <div className="w-[90%] justify-start items-start gap-2">
                     <h1 className="text-white text-[40px]">Welcome to Cashtree Tap-To-Win</h1>
                     <p className="text-white text-lg">The exciting Web3 tapping game where you can compete to win a massive prize pool of up to 75,000,000 $CTT with a 1000 $CTT entry fee.🏆</p>
