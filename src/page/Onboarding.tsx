d323fd226ad4d883663149d8d6fe15ec942b41c3
Senior Coder <agilecoder2040@gmail.com>
2024-08-10 16:02:52 +0900

    d

--- src/page/Onboarding.tsx
+++ src/page/Onboarding.tsx
@@ -24,7 +24,7 @@ const Onboarding = () => {
                                     <img src="/image/onboarding/onboarding_5.png" alt="" className="w-auto h-full" />}
 
                 </div>
-                <div className="w-full h-[50%] flex justify-center items-center">
+                <div className="w-full h-[40%] flex justify-center items-center">
                     {pageNum == 1 ?
                         <div className="w-[90%] h-full flex flex-col justify-start items-start gap-2">
                             <h1 className="text-white text-[40px] text-left">Welcome to Cashtree Tap-To-Win</h1>
