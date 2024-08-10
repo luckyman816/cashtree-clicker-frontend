0171dcb276c0fa240f6db9aee4ec34f0b7353278
Senior Coder <agilecoder2040@gmail.com>
2024-08-10 17:23:37 +0900

    d

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `https://cashtree-clicker-backend.onrender.com/api`,
+    baseURL: `http://localhost:5000/api`,
     headers: {
       'Content-Type': 'application/json'
     }
