b471f74d3ca90b7fd7e4a70566a3a3b1e8fd8063
Senior Coder <agilecoder2040@gmail.com>
2024-08-11 17:40:47 +0900

    d

--- src/page/Onboarding.tsx
+++ src/page/Onboarding.tsx
@@ -8,7 +8,7 @@ const Onboarding = () => {
         setPageNum((prevNum) => prevNum + 1)
     }
     return (
-        <div className=" relative w-screen h-screen flex flex-col justify-around items-center bg-[linear-gradient(0deg,_var(--tw-gradient-stops))] from-[#120F29] to-[#7F3AEF]" >
+        <div className=" relative w-screen h-screen flex flex-col justify-around items-center bg-[linear-gradient(0deg,_var(--tw-gradient-stops))] from-[#120F29] to-[#7F3AEF] py-5" >
             <div className=" absolute w-full h-full right-0 top-0 z-10" style={{ backgroundImage: "url(/image/onboarding/gradient.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
             </div>
             <div className="w-full h-full justify-around items-center flex flex-col gap-2 z-20">
