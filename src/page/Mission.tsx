9821c47722a1014dc27b00a0c110b140b4945d79
Blue Soul <bluesoul2130758@gmail.com>
2024-08-12 14:01:42 -0700

    d

--- src/page/Mission.tsx
+++ src/page/Mission.tsx
@@ -414,24 +414,14 @@ export default function Mission() {
               className={`flex w-[90%] my-3 px-3 py-3 items-center justify-between bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#4756E5] to-[#47C8FF] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)]  rounded-[20px] gap-2 border-none`}
               onClick={() => handleOpenDailyTaskModal("secretExtra")}
             >
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center gap-3">
                 <img
                   src={`/image/mission/secretExtra.png`}
                   alt=""
                   className="w-10 h-10"
                 />
-                <div className="flex flex-col gap-1 justify-start items-start">
-                  <h3 className="text-sm text-white">Redeem Secret Code</h3>
-                  <div className="flex justify-center items-center">
-                    <img
-                      src="/image/assets/coin.png"
-                      alt="coin"
-                      className="w-4 h-4"
-                    />
-                    <h3 className="text-[13px] text-white font-bold">
-                      +{formatNumberWithCommas(25000)}
-                    </h3>
-                  </div>
+                <div className="flex flex-col gap-1 justify-start items-center">
+                  <h3 className="text-base text-white">Redeem Secret Code</h3>
                 </div>
               </div>
               <img
@@ -449,7 +439,7 @@ export default function Mission() {
                 className={`flex w-[90%] my-3 px-3 py-3 items-center justify-between bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#A944FD] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)]  rounded-[20px] gap-2 border border-[#B286FA]`}
                 onClick={() => handleOpenDailyTaskModal(item.icon)}
               >
-                <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center gap-3">
                   <img
                     src={`/image/mission/${item.icon}.png`}
                     alt=""
@@ -486,7 +476,7 @@ export default function Mission() {
                 className={`flex w-[90%] my-3 px-3 py-3 items-center justify-between bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#A944FD] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)]  rounded-[20px] gap-2 border border-[#B286FA]`}
                 onClick={() => handleOpenTaskListModal(item.icon)}
               >
-                <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center gap-3">
                   <img
                     src={`/image/mission/${item.icon}.png`}
                     alt=""
