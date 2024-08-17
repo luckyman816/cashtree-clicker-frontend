4ce12452138d897919c2266ca533b54a05c6bb24
Blue Soul <bluesoul2130758@gmail.com>
2024-08-16 20:41:18 -0700

    s

--- src/page/Boost.tsx
+++ src/page/Boost.tsx
@@ -1,12 +1,16 @@
 import { dispatch, useSelector } from "../store";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import {
   updateWallet,
   getWallet,
   updateBalance,
 } from "../store/reducers/wallet";
 // import {levelTargets, energyLimit} from "../data";
-import { getDailyBoost, updateDoublePoints, updateRefillEnergy } from "../store/reducers/dailyBoost";
+import {
+  getDailyBoost,
+  updateDoublePoints,
+  updateRefillEnergy,
+} from "../store/reducers/dailyBoost";
 // import { updateTapLevel, updateLimit } from "../store/reducers/wallet";
 import { useEffect, useState } from "react";
 import Modal from "../component/modal";
@@ -19,26 +23,34 @@ export default function Boost() {
   const tapLevelState = useSelector((state) => state.wallet.user?.tap_level);
   const refill_energy_state = useSelector(
     (state) => state.dailyBoost.daily_refill_energy?.refill_energy
-  )
+  );
   const double_points_state = useSelector(
     (state) => state.dailyBoost.daily_double_points?.double_points
-  )
+  );
   const refill_energy_date_state = useSelector(
     (state) => state.dailyBoost.daily_refill_energy?.refill_energy_date
-  )
+  );
   const double_points_date_state = useSelector(
     (state) => state.dailyBoost.daily_double_points?.double_points_date
-  )
+  );
   const [token, setToken] = useState<number>(tokenState);
   const [username, setUsername] = useState<string>(username_state);
   const [limit, setLimit] = useState<number>(limit_state);
-  const [refill_energy, setRefillEnergy] = useState<number>(refill_energy_state);
-  const [refill_energy_date, setRefillEnergyDate] = useState<moment.Moment | null>(refill_energy_date_state ? moment(refill_energy_date_state) : null);
-  const [double_points, setDoublePoints] = useState<number>(double_points_state);
-  const [double_points_date, setDoublePointsDate] = useState<moment.Moment | null>(double_points_date_state ? moment(double_points_date_state) : null);
+  const [refill_energy, setRefillEnergy] =
+    useState<number>(refill_energy_state);
+  const [refill_energy_date, setRefillEnergyDate] =
+    useState<moment.Moment | null>(
+      refill_energy_date_state ? moment(refill_energy_date_state) : null
+    );
+  const [double_points, setDoublePoints] =
+    useState<number>(double_points_state);
+  const [double_points_date, setDoublePointsDate] =
+    useState<moment.Moment | null>(
+      double_points_date_state ? moment(double_points_date_state) : null
+    );
   useEffect(() => {
     dispatch(getWallet(username));
-    dispatch(getDailyBoost(username))
+    dispatch(getDailyBoost(username));
   }, [username]);
   useEffect(() => {
     setToken(tokenState);
@@ -46,9 +58,22 @@ export default function Boost() {
     setLimit(limit_state);
     setRefillEnergy(refill_energy_state);
     setDoublePoints(double_points_state);
-    setRefillEnergyDate(refill_energy_date_state ? moment(refill_energy_date_state) : null);
-    setDoublePointsDate(double_points_date_state ? moment(double_points_date_state) : null);
-  }, [tokenState, username_state, limit_state, tapLevelState, refill_energy_state, double_points_state, refill_energy_date_state, double_points_date_state]);
+    setRefillEnergyDate(
+      refill_energy_date_state ? moment(refill_energy_date_state) : null
+    );
+    setDoublePointsDate(
+      double_points_date_state ? moment(double_points_date_state) : null
+    );
+  }, [
+    tokenState,
+    username_state,
+    limit_state,
+    tapLevelState,
+    refill_energy_state,
+    double_points_state,
+    refill_energy_date_state,
+    double_points_date_state,
+  ]);
   function formatNumberWithCommas(number: number, locale = "en-US") {
     return new Intl.NumberFormat(locale).format(number);
   }
@@ -85,7 +110,7 @@ export default function Boost() {
       } else {
         if (token > 3000) {
           dispatch(updateRefillEnergy(username, refill_energy + 1, moment()));
-          dispatch(updateWallet(username, (token - 3000), limit));
+          dispatch(updateWallet(username, token - 3000, limit));
           // for (let i: number = 0; i < levelTargets.length; i++) {
           //   if ((token - 3000) < levelTargets[i]) {
           //     dispatch(updateTapLevel(username, i));
@@ -94,12 +119,12 @@ export default function Boost() {
           //   }
           // }
           toast.success("Refilled successfully");
-        } else  {
+        } else {
           toast.error("Insufficient balance!");
         }
       }
     } else {
-      toast.error("Please wait next day!")
+      toast.error("Please wait next day!");
     }
     setIsRefillEnergyModalOpen(false);
   };
@@ -110,7 +135,7 @@ export default function Boost() {
       } else {
         if (diffMinutesDouble >= 15) {
           dispatch(updateDoublePoints(username, double_points + 1, moment()));
-          dispatch(updateBalance(username, token + 500))
+          dispatch(updateBalance(username, token + 500));
           // for (let i: number = 0; i < levelTargets.length; i++) {
           //   if ((token + 500) < levelTargets[i]) {
           //     dispatch(updateTapLevel(username, i));
@@ -124,10 +149,10 @@ export default function Boost() {
         }
       }
     } else {
-      toast.error("Please wait next day!")
+      toast.error("Please wait next day!");
     }
     setIsDoublePointsModalOpen(false);
-  }
+  };
   useEffect(() => {
     const interval = setInterval(() => {
       if (diffDaysRefill > 0) {
@@ -138,15 +163,17 @@ export default function Boost() {
       }
     }, 1000);
     return () => clearInterval(interval);
-  }, [])
-  const [isRefillEnergyModalOpen, setIsRefillEnergyModalOpen] = useState<boolean>(false);
+  }, []);
+  const [isRefillEnergyModalOpen, setIsRefillEnergyModalOpen] =
+    useState<boolean>(false);
   const handleMouseClick = () => {
     setIsRefillEnergyModalOpen(true);
   };
   const handleCloseRefillEnergyModal = () => {
     setIsRefillEnergyModalOpen(false);
   };
-  const [isDoublePointsModalOpen, setIsDoublePointsModalOpen] = useState<boolean>(false);
+  const [isDoublePointsModalOpen, setIsDoublePointsModalOpen] =
+    useState<boolean>(false);
   const handleOpenDoublePointsModal = () => {
     setIsDoublePointsModalOpen(true);
   };
@@ -167,65 +194,102 @@ export default function Boost() {
           </h1>
         </div>
         <div className="flex justify-start">
-          <h1 className="text-white text-xl">Buy Boosters&nbsp;&nbsp;<span className="text-white text-sm">Reset at 00:00 AM UTCC</span></h1>
+          <h1 className="text-white text-xl">
+            Buy Boosters&nbsp;&nbsp;
+            <span className="text-white text-sm">Reset at 00:00 AM UTCC</span>
+          </h1>
         </div>
-        <div
-          className={`w-full flex my-3 px-3 py-3 justify-between items-center bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#A944FD] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)] rounded-[20px] gap-2`}
-          onClick={handleMouseClick}
-        >
-          <div className="flex justify-center items-center gap-1">
-            <img src="/image/assets/fillEnergy.webp" alt="" className="w-10 h-10" />
-            <div className="flex flex-col gap-1 justify-start items-start">
-              <h3 className="text-sm text-white">Energy Refrill 3000 P</h3>
-              <h3 className="text-[13px] text-white">
-                {refill_energy}/3 available
-              </h3>
+        <div className="flex flex-col gap-2 w-full my-3">
+          <div
+            className={`w-full flex px-3 py-3 justify-between items-center bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#AE47FF] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)] rounded-[20px] gap-2`}
+            onClick={handleMouseClick}
+          >
+            <div className="flex justify-center items-center gap-1">
+              <img
+                src="/image/assets/fillEnergy.webp"
+                alt=""
+                className="w-10 h-10"
+              />
+              <div className="flex flex-col gap-1 justify-start items-start">
+                <h3 className="text-sm text-white">Energy Refrill 3000 P</h3>
+                <h3 className="text-[13px] text-white">
+                  {refill_energy}/3 available
+                </h3>
+              </div>
             </div>
+            <img src="image/icon/arrowRight.webp" alt="" className="w-2 h-4" />
           </div>
-          <img src="image/icon/arrowRight.webp" alt="" className="w-2 h-4" />
-        </div>
-        <div
-          className={`w-full flex my-3 px-3 py-3 justify-between items-center bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6929F1] to-[#A944FD] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)]  rounded-[20px] gap-2`}
-          onClick={handleOpenDoublePointsModal}>
-          <div className="flex justify-center items-center gap-1">
-            <img src="/image/assets/doublePoint.webp" alt="" className="w-10 h-10 drop-shadow-[0_20px_50px_#7131D6]" />
-            <div className="flex flex-col gap-1 justify-start items-start">
-              <h3 className="text-sm text-white">Double Points for 15 Minutes 5000 P</h3>
-              <h3 className="text-[13px] text-white">
-                {double_points}/3 available
-              </h3>
+          <div
+            className={`w-full flex px-3 py-3 justify-between items-center bg-[linear-gradient(315deg,_var(--tw-gradient-stops))] from-[#6E1BD8] to-[#D940FF] hover:bg-[linear-gradient(0.5turn, #711CD9, #CD3CFB)]  rounded-[20px] gap-2`}
+            onClick={handleOpenDoublePointsModal}
+          >
+            <div className="flex justify-center items-center gap-1">
+              <img
+                src="/image/assets/doublePoint.webp"
+                alt=""
+                className="w-10 h-10 drop-shadow-[0_20px_50px_#7131D6]"
+              />
+              <div className="flex flex-col gap-1 justify-start items-start">
+                <h3 className="text-sm text-white">
+                  Double Points for 15 Minutes 5000 P
+                </h3>
+                <h3 className="text-[13px] text-white">
+                  {double_points}/3 available
+                </h3>
+              </div>
             </div>
+            <img src="image/icon/arrowRight.webp" alt="" className="w-2 h-4" />
           </div>
-          <img src="image/icon/arrowRight.webp" alt="" className="w-2 h-4" />
         </div>
       </div>
       <Footer />
-      <Modal isOpen={isRefillEnergyModalOpen} onClose={handleCloseRefillEnergyModal}>
+      <Modal
+        isOpen={isRefillEnergyModalOpen}
+        onClose={handleCloseRefillEnergyModal}
+      >
         <div className="flex flex-col items-center align-middle rounded-[20px] gap-5 my-5 h-full">
-          <img src="image/assets/fillEnergyModal.webp" alt="" className=" w-auto h-[50%] drop-shadow-[0_20px_50px_#7131D6]" />
+          <img
+            src="image/assets/fillEnergyModal.webp"
+            alt=""
+            className=" w-auto h-[50%] drop-shadow-[0_20px_50px_#7131D6]"
+          />
           <h1 className="text-2xl text-white">Energy Refill</h1>
           <p className=" text-sm text-white">
-            Refill your energy bar instantly for 3,000 points. Maximum of 3 purchases per day
+            Refill your energy bar instantly for 3,000 points. Maximum of 3
+            purchases per day
           </p>
           <div
             className="w-[80%] bg-[#7520FF] text-white rounded-[10px] flex justify-center items-center py-3"
             onClick={handleRefillEnergy}
           >
-            <span className="flex justify-center items-center text-white text-xl">Get</span>
+            <span className="flex justify-center items-center text-white text-xl">
+              Get
+            </span>
           </div>
         </div>
       </Modal>
-      <Modal isOpen={isDoublePointsModalOpen} onClose={handleCloseDoublePointsModal}>
+      <Modal
+        isOpen={isDoublePointsModalOpen}
+        onClose={handleCloseDoublePointsModal}
+      >
         <div className="flex flex-col items-center align-middle rounded-[20px] gap-5 my-5 h-full">
-          <img src="image/assets/doublePointsModal.webp" alt="" className=" w-auto h-[50%]" />
+          <img
+            src="image/assets/doublePointsModal.webp"
+            alt=""
+            className=" w-auto h-[50%]"
+          />
           <h1 className="text-2xl text-white">Double Points</h1>
           <p className=" text-sm text-white">
-            Double your points earned for the next 15 minutes for 5,000 points. Maximum of 3 purchases per day.
+            Double your points earned for the next 15 minutes for 5,000 points.
+            Maximum of 3 purchases per day.
           </p>
           <div
-            className="w-[80%] bg-[#7520FF] text-white rounded-[10px] flex justify-center items-center py-3" onClick={handleDoublePoints}
+            className="w-[80%] bg-[#7520FF] text-white rounded-[10px] flex justify-center items-center py-3"
+            onClick={handleDoublePoints}
           >
-            <span className="flex justify-center items-center text-white text-xl">Get</span>
+            <span className="flex justify-center items-center text-white text-xl">
+              Get
+            </span>
           </div>
         </div>
       </Modal>
